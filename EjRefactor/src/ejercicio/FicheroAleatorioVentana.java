f32c64a75d2e92386249155592b6a6f985fe178b
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-03-15 22:06:18 +0100

    ejercicioRefactor terminado

---
+++ EjRefactor/src/ejercicio/FicheroAleatorioVentana.java
@@ -0,0 +1,10 @@
+package ejercicio;
+
+public class FicheroAleatorioVentana {
+
+	public static void main(String[] args) {
+
+		VentanaDepartamento ventanaDepart = new VentanaDepartamento();
+		ventanaDepart.setVisible(true);
+	}
+}
